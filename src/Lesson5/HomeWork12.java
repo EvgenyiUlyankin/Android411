f54dfadb740f6d4fa41474618d98954c21735c13
Evgenyi Ulyankin <ulyankin1986@gmail.com>
2024-11-17 18:02:39 +0300

    change in HomeWork12

--- src/Lesson5/HomeWork12.java
+++ src/Lesson5/HomeWork12.java
@@ -8,14 +8,14 @@ public class HomeWork12 {
 
         System.out.print("Введите значение n: ");
         int n = input.nextInt();
-        int h = 3;
-        int w = n * 2-4;
 
         one_str(n);
-        middle_str(h, w);
+
+        for (int i = 0; i < n-2; i++) {
+            middle_str(n);
+        }
+
         last_str(n);
-
-
 
     }
     public static void one_str(int n){
@@ -25,22 +25,22 @@ public class HomeWork12 {
         System.out.println();
     }
 
-    public static void middle_str(int h, int w){
-        String ch = "\\/";
-        for (int i = 0; i < h ; i++ ) {
-            for (int j = 0; j < w ; j++) {
-                if (i == 0 || i == h || j == 0 || j == w-1)
-                    System.out.print("=");
-                else
-                    System.out.print(ch);
-            }
-            System.out.println();
-        }
-    }
+    public static void middle_str(int n){
+        System.out.print("=");
 
+        for (int i = 0; i < n-1; i++) {
+            System.out.print("\\/");
+
+        }
+        System.out.print("=");
+
+        System.out.println();
+         }
     public static void last_str(int n){
         for (int i = 0; i <n*2 ; i++) {
             System.out.print("=");
         }
     }
 }
+
+
