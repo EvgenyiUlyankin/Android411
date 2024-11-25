96305f25688c6e9574c5ef93ff083649d42064e1
Evgenyi Ulyankin <ulyankin1986@gmail.com>
2024-11-25 23:42:34 +0300

    Home Work

--- src/HomeWorkForGit/HomeWork13.java
+++ src/HomeWorkForGit/HomeWork13.java
@@ -1,4 +1,26 @@
 package HomeWorkForGit;
 
+import java.util.Scanner;
+
 public class HomeWork13 {
+    public static void main(String[] args) {
+
+//        Scanner input = new Scanner(System.in);
+//
+//        int[] mas = {-2, 3, 8, -11, -4, 6};
+//         System.out.println("n = " + number(0, mas));
+//    }
+//
+//    public static int number (int n, int[] mas) {
+//        for (int i = 0; i < mas.length; i++) {
+//            if (mas[i] < 0) n++;
+//
+//            return number(n, mas);
+//        }
+//
+//         return n;
+
+        System.out.println("Склонированный репозиторий");
+        System.out.println("Домашнее задание");
+    }
 }
